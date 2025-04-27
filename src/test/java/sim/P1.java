4cbd3c895925b4b0df9044c2a6e9de136ff29bf6
ss1864510 <shaiksubhani.basheer@gmail.com>
2025-04-27 12:31:58 +0530

    New changess ss

--- src/test/java/sim/P1.java
+++ src/test/java/sim/P1.java
@@ -1,7 +1,10 @@
 package sim;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -23,5 +26,31 @@ public class P1 {
 		Assert.assertEquals("SEARCH", tes);
 		
 	}
+	@Test 
+	public void s3() {
+		ChromeOptions option=new ChromeOptions();
+		WebDriver driver =new ChromeDriver(option);
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		List<WebElement> ls=
+				driver.findElements
+				(By.xpath("//div[@class='w3-white w3-padding notranslate w3-padding-16']/table/tbody/tr[2]"));
+		
+		for(WebElement l:ls) {
+			System.out.println(l.getText());
+		}
+	}
+		@Test
+		public void s4() {
+		ChromeOptions option=new ChromeOptions();
+		WebDriver driver =new ChromeDriver(option);
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		List<WebElement> ls=
+				driver.findElements
+				(By.xpath("//div[@class='w3-white w3-padding notranslate w3-padding-16']/table/tbody/tr/td[1]"));
+		
+		for(WebElement l:ls) {
+			System.out.println(l.getText());
+		
+	}}
 
 }
